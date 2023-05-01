5c0ca29d44888915aa10dae7388c08e9e387a48d
sdfmahmood14 <sdfmahmood14@gmail.com>
2023-05-02 00:37:35 +0530

    horizontal and vertical with toggle button

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+const liEl = document.querySelectorAll('#li');
+const toggleBtnEl = document.getElementById('toggle-btn');
+const divEl = document.getElementById('optional-content');
+toggleBtnEl.addEventListener('click',() => {
+    for(const el of liEl){
+    el.classList.toggle("li-vt");}
+    divEl.classList.toggle("vertical-div");
+})
